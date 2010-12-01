ae861638905b91ad6ef03a89736e1a75bad6fea9
Charles <cuy@g2ix.net>
2010-12-01 15:59:35 +0800

    * contains application key, secret and urban airship url

---
+++ Classes/UrbanAirshipDemoConstants.h
@@ -0,0 +1,14 @@
+/*
+ *  UrbanAirshipDemoConstants.h
+ *  UrbanAirshipDemo
+ *
+ *  Created by Charles Joseph Uy on 12/1/10.
+ *  Copyright 2010 G2iX. All rights reserved.
+ *
+ */
+
+
+// From UrbanAirship Account
+#define URBANAIRSHIP                @"https://go.urbanairship.com"
+#define APPLICATION_KEY             @"P2q14VtyTyGejxK-UuD-GA"
+#define APPLICATION_SECRET          @"sr5mxVVGR_Gy5bLxewUC6Q"
