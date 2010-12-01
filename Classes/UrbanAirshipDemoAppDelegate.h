99b2b8e758d1c49edb1763f797a0b00b1692bdc0
Charles <cuy@g2ix.net>
2010-12-01 14:35:41 +0800

    * initial project files

---
+++ Classes/UrbanAirshipDemoAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  UrbanAirshipDemoAppDelegate.h
+//  UrbanAirshipDemo
+//
+//  Created by Charles Joseph Uy on 12/1/10.
+//  Copyright 2010 G2iX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UrbanAirshipDemoAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
